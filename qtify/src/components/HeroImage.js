b0424a45f8429b24477a4602f69423b9bbcd4b47
dineshchintu <mdinesh@iitg.ac.in>
2023-12-28 21:59:15 +0530

    heroImage2

--- qtify/src/components/HeroImage.js
+++ qtify/src/components/HeroImage.js
@@ -4,6 +4,15 @@ import './HeroImage.css'
 export default function HeroImage(){
     return (
         <Box className='hero' >
+            <Box className="hero-heading">
+            <p >
+            100 Thousand Songs, ad-free
+            </p>
+            <p >
+            Over thousands podcast episodes
+            </p>
+            </Box>
+
         </Box>
     )
 }
